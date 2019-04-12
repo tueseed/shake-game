08b5cf5e765835b73cafba33efbcd161e82ffdbf
Nutthapong <tue_seed@hotmail.com>
2019-04-12 12:50:26 +0700

    controlfunction

--- result.php
+++ result.php
@@ -23,29 +23,10 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xs-3 text-center bg1">
-                    <div style="margin-top:150px;margin-bottom:50px">
-                        <img src="images/coin1.png" width="70%" alt="">
-                    </div>
-                    <div class="col-xs-10 col-xs-offset-1" style="color:#fff"><h3>Team Score</h3></div>
-                    <div class="col-xs-10 col-xs-offset-1 scoreShow" id="team1score"><h1>0</h1></div>
-                    <div style="margin-top:600px"></div>
+                    <input type="button" class="btn btn-success" onclick="control()">
                 </div>
-                <div class="col-xs-3 text-center bg2">
-                    <div style="margin-top:150px;margin-bottom:50px">
-                        <img src="images/coin3.png" width="70%" alt="">
-                    </div>
-                    <div class="col-xs-10 col-xs-offset-1" style="color:#fff"><h3>Team Score</h3></div>
-                    <div class="col-xs-10 col-xs-offset-1 scoreShow" id="team2score"><h1>0</h1></div>
-                    <div style="margin-top:600px"></div>
-                </div>
-                <div class="col-xs-3 text-center bg3">
-                    <div style="margin-top:150px;margin-bottom:50px">
-                        <img src="images/coin2.png" width="70%" alt="">
-                    </div>
-                    <div class="col-xs-10 col-xs-offset-1" style="color:#fff"><h3>Team Score</h3></div>
-                    <div class="col-xs-10 col-xs-offset-1 scoreShow" id="team3score"><h1>0</h1></div>
-                    <div style="margin-top:600px"></div>
-                </div>
+                
+                
                 <div class="col-xs-3 text-center bg4">
                     <div style="margin-top:150px;margin-bottom:50px">
                         <img src="images/coin4.png" width="70%" alt="">
@@ -65,68 +46,35 @@
         <script>  
             // Initialize Firebase
             var config = {
-                apiKey: //ใส่ API KEY ตรงนี้,
-                authDomain: //ใส่ authDomain ตรงนี้,
-                databaseURL: //ใส่ Database URL ตรงนี้,
-                projectId: //ใส่ projectID ตรงนี้,
+                apiKey: 'AIzaSyC16zgNqr4Tb9rZ86n_Z0Ht6C70Xwc_NGE',
+                authDomain: 'shake-game-bfd34.firebaseapp.com',
+                databaseURL: 'https://shake-game-bfd34.firebaseio.com',
+                projectId: 'shake-game-bfd34',
             };
             firebase.initializeApp(config);
+            function control()
+            {
+                var score = firebase.database().ref('control');
+                score.on('value', function(snapshot) {
+                                                    var status = snapshot.val();
+                                                    if(status == 'off')
+                                                    {
+                                                        firebase.database().ref('control').set('on');
+                                                    }
+                                                    else if(status == 'on')
+                                                    {
+                                                        firebase.database().ref('control').set('off');
+                                                    }
+                                                }
+                    );                          
+            }
+            
 
-            var team1 = firebase.database().ref('1/');
-            team1.on('value', function(snapshot) {
-                var num=Object.keys(snapshot.val()).length;
-                scoreTeam1=0;
-                for(i=0;i<num;i++){
-                    var user=Object.keys(snapshot.val())[i];
-                    var scoreNode = firebase.database().ref('1/'+user+'/score/');
-                    scoreNode.once('value').then(function(snapshot2){
-                        scoreTeam1=scoreTeam1+snapshot2.val();
-                        $("#team1score").html("<h1>"+scoreTeam1+"</h1>");
-                    });
-                }                                
-            });
+            
 
-            var team2 = firebase.database().ref('2/');
-            team2.on('value', function(snapshot) {
-                var num=Object.keys(snapshot.val()).length;
-                scoreTeam2=0;
-                for(b=0;b<num;b++){
-                    var user=Object.keys(snapshot.val())[b];
-                    var scoreNode = firebase.database().ref('2/'+user+'/score/');
-                    scoreNode.once('value').then(function(snapshot2){
-                        scoreTeam2=scoreTeam2+snapshot2.val();
-                        $("#team2score").html("<h1>"+scoreTeam2+"</h1>");
-                    });
-                }                                
-            });
+           
 
-            var team3 = firebase.database().ref('3/');
-            team3.on('value', function(snapshot) {
-                var num=Object.keys(snapshot.val()).length;
-                scoreTeam3=0;
-                for(g=0;g<num;g++){
-                    var user=Object.keys(snapshot.val())[g];
-                    var scoreNode = firebase.database().ref('3/'+user+'/score/');
-                    scoreNode.once('value').then(function(snapshot2){
-                        scoreTeam3=scoreTeam3+snapshot2.val();
-                        $("#team3score").html("<h1>"+scoreTeam3+"</h1>");
-                    });
-                }                                
-            });
-
-            var team4 = firebase.database().ref('4/');
-            team4.on('value', function(snapshot) {
-                var num=Object.keys(snapshot.val()).length;
-                scoreTeam4=0;
-                for(m=0;m<num;m++){
-                    var user=Object.keys(snapshot.val())[m];
-                    var scoreNode = firebase.database().ref('4/'+user+'/score/');
-                    scoreNode.once('value').then(function(snapshot2){
-                        scoreTeam4=scoreTeam4+snapshot2.val();
-                        $("#team4score").html("<h1>"+scoreTeam4+"</h1>");
-                    });
-                }                                
-            });
+            
 
            
         </script>
